1f5ca103b45eaece8a5cb3d93ff4795362ac743a
Cody Washington <115746975+Jelatinone@users.noreply.github.com>
2026-01-17 21:57:51 -0600

    Partially Working local SQL for login links

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import EmailProvider from "next-auth/providers/email";
+import { prisma } from "../../lib/prisma";
 
 export const authOptions = {
 	providers: [
@@ -9,11 +11,11 @@ export const authOptions = {
 			maxAge: 24 * 60 * 60,
 		}),
 	],
+	adapter: PrismaAdapter(prisma),
 	secret: process.env.NEXTAUTH_SECRET,
 	session: {
-		strategy: "jwt",
+		strategy: "database",
 	},
-	adapter: undefined,
 	pages: {
 		signIn: "/login",
 	},
